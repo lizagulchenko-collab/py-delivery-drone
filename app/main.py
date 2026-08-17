5fb22410b5259e125c6fc9777b0004def56fb300
Yelyzaveta Hulchenko <liza.gulchenko@gmail.com>
2026-08-17 13:13:26 +0300

    Fixed flake8

--- app/main.py
+++ app/main.py
@@ -14,16 +14,16 @@ class BaseRobot:
         self.weight = weight
         self.coords = coords
 
-    def go_forward(self, step: int=1) -> None:
+    def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
 
-    def go_back(self, step: int=1) -> None:
+    def go_back(self, step: int = 1) -> None:
         self.coords[1] += step
 
-    def go_right(self, step: int=1) -> None:
+    def go_right(self, step: int = 1) -> None:
         self.coords[0] += step
 
-    def go_left(self, step: int=1) -> None:
+    def go_left(self, step: int = 1) -> None:
         self.coords[0] -= step
 
     def get_info(self) -> str:
@@ -39,10 +39,10 @@ class FlyingRobot(BaseRobot):
             coords = [0, 0, 0]
         super().__init__(name, weight, coords)
 
-    def go_up(self, step: int=1) -> None:
+    def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
 
-    def go_down(self, step: int=1) -> None:
+    def go_down(self, step: int = 1) -> None:
         self.coords[2] -= step
 
 
