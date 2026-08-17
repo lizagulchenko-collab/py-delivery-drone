3ba2c38a51a7ff8d3f4b062e02f016c933d078d2
Yelyzaveta Hulchenko <liza.gulchenko@gmail.com>
2026-08-17 13:24:29 +0300

    Fixed typo

--- app/main.py
+++ app/main.py
@@ -56,13 +56,13 @@ class DeliveryDrone(FlyingRobot):
         if coords is None:
             coords = [0, 0, 0]
         super().__init__(name, weight, coords)
-        self.maw_load_weight = max_load_weight
-        self.current_load = current_load
+        self.max_load_weight = max_load_weight
+        self.current_load = None
         if current_load is not None:
             self.hook_load(current_load)
 
     def hook_load(self, cargo: Cargo) -> None:
-        if self.current_load is None and cargo.weight <= self.maw_load_weight:
+        if self.current_load is None and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
 
     def unhook_load(self) -> None:
